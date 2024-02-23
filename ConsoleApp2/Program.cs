a2b0a8037aecbae8f5b87c7e5933a39f56bc79c3
spraws <145443721+spraws@users.noreply.github.com>
2024-02-23 19:56:38 +0000

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables
+            int no1;
+            int no2;
+            int choice;
+
+            Console.WriteLine("Enter a number!");
+            no1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter another number");
+            no2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Press 1 to add, 2 to multiply, 3 to subtract or 4 to divide");
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                int value;
+                value = no1 + no2;
+                Console.WriteLine(value);
+            }
+            if (choice == 2)
+            {
+                int value;
+                value = no1 * no2;
+                Console.WriteLine(value);
+            }
+            if (choice ==3)
+            {
+                int value;
+                value = no1 - no2;
+                Console.WriteLine(value);
+            }
+
+        }
+    }
+}
